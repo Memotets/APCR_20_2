93c89f85f51a772a1fd005983e4593a1e0a0840b
Alumno <Alumno@LAB03-PC11>
2020-01-23 09:56:32 -0600

    Server ready

--- Proyecto_Unidad_1/src/PrimerClienteServidorTCP/ClienteTCP.java
+++ Proyecto_Unidad_1/src/PrimerClienteServidorTCP/ClienteTCP.java
@@ -4,11 +4,83 @@
  * and open the template in the editor.
  */
 package PrimerClienteServidorTCP;
-
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Alumno
  */
 public class ClienteTCP {
+    
+    private Socket clint;
+    private InputStream in;
+    private OutputStream out;
+    private DataInputStream msnIn;
+    private DataOutputStream msnOut;
+    private int host_port;
+    private String host_ip;
+    
+    
+    public ClienteTCP(int p, String ip){
+        this.host_port= p;
+        this.host_ip=ip;
+    }
+    public static void main (String []ad){
+       test();
+       test();
+       test();
+       
+    }
+    public static void test(){
+        ClienteTCP wop = new ClienteTCP(1234, "127.0.0.1");
+        wop.openClient();
+        wop.recive();
+        wop.send("Hola, soy nuevo");
+        wop.closeClient();
+    }
+    
+    public void openClient(){
+        try {
+            this.clint= new Socket(this.host_ip,this.host_port);
+            //this.clint.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public void closeClient(){
+        try {
+           // this.clint= new Socket(this.host_ip,this.host_port);
+            this.clint.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    
+    public void send (String msn){
+        try {
+            //Flujo de salida
+            this.out = this.clint.getOutputStream();
+             // enviar mensaje al cliente
+            msnOut = new DataOutputStream(out);
+            msnOut.writeUTF(this.host_ip+": "+msn);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    public void recive (){
+        try {
+            //Flujo de entrada
+            this.in = this.clint.getInputStream();
+            //mensaje de entrada
+            this.msnIn = new DataInputStream(in);
+            System.out.println(this.msnIn.readUTF());
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
--- Proyecto_Unidad_1/src/PrimerClienteServidorTCP/ServidorTCP.java
+++ Proyecto_Unidad_1/src/PrimerClienteServidorTCP/ServidorTCP.java
@@ -13,30 +13,83 @@ import java.util.logging.Logger;
  * @author Alumno
  */
 public class ServidorTCP {
-    private ServerSocket servidor;
-    private Socket cliente;
-    private InputStream entrada;
-    private OutputStream salida;
+    private ServerSocket serv;
+    private Socket clint;
+    private InputStream in;
+    private OutputStream out;
     private String ip, protocolo;
-    private int puerto;
-    private DataInputStream mensajeEntrada;
-    private DataOutputStream mensajeSalida;
+    private int port;
+    private DataInputStream msnIn;
+    private DataOutputStream msnOut;
     
     public ServidorTCP(){
+        this.port = 1234;
+    }
+    public void openServ(){
         try {
-            puerto = 1234;
-            servidor = new ServerSocket(puerto);
+            this.serv = new ServerSocket(port);
             System.out.println("Servidor a la escucha...");
-            cliente = servidor.accept();
-            InetAddress ipCliente = cliente.getInetAddress();
-            System.out.println("Se conecto: " + ipCliente.getHostAddress());
-            entrada = cliente.getInputStream();
-            salida = cliente.getOutputStream();
-            // enviar mensaje al cliente
-            mensajeSalida = new DataOutputStream(salida);
-            mensajeSalida.writeChars("Voy por la banqueta Perro D:<");
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-    } 
+    }
+    public void closeServ(){
+        try {
+           // this.clint= new Socket(this.host_ip,this.host_port);
+            this.serv.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public static void main (String []ad){
+        ServidorTCP wop = new ServidorTCP();
+        wop.openServ();
+        while (true){
+            wop.clientConnected();
+            wop.send("Bienvenido al infierno... gustas galletas(?)");
+            wop.recive();
+            wop.clientDisconnected();
+        }
+    }
+    public void clientConnected(){
+        try {
+            this.clint = serv.accept();
+            InetAddress ipCliente = clint.getInetAddress();
+            System.out.println("Se conecto: " + ipCliente.getHostAddress());
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public void clientDisconnected(){
+        try {
+           // this.clint= new Socket(this.host_ip,this.host_port);
+            this.clint.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+    public void send (String msn){
+        try {
+            //Flujo de salida
+            this.out = this.clint.getOutputStream();
+             // enviar mensaje al cliente
+            msnOut = new DataOutputStream(out);
+            msnOut.writeUTF("Host: "+msn);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    public void recive (){
+        try {
+            //Flujo de entrada
+            this.in = this.clint.getInputStream();
+            //mensaje de entrada
+            this.msnIn = new DataInputStream(in);
+            System.out.println(this.msnIn.readUTF());
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteTCP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
